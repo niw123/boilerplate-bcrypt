0a15ae62cf875e52cb58c896370a2f02f057806f
Naruto Iwasaki <niw206@gmail.com>
2018-10-26 18:50:48 -0700

    Finish bcrypt exercises

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 const express     = require('express');
 const bodyParser  = require('body-parser');
+const bcrypt      = require('bcrypt')
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
 
@@ -12,12 +13,25 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  if (err) {
+    console.log(`Error computing the hash: ${err}`)
+  }
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    if (err) {
+      console.log(`Error comparing the hash: ${err}`)
+    }
+    console.log(res);
+  })
+})
 
 
 //END_ASYNC
 
 //START_SYNC
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+const res = bcrypt.compareSync(myPlaintextPassword, hash)
+console.log(res)
 
 
 
